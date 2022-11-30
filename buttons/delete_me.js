689de849a42311ee55eafd1ddd4e56e2f6a09d13
Lozarth <monkojumboepicness@gmail.com>
2022-11-30 20:48:07 +0000

    Legacy

---
+++ buttons/delete_me.js
@@ -0,0 +1,20 @@
+const { ButtonStyle, ActionRowBuilder, ButtonBuilder } = require('discord.js')
+
+module.exports = {
+    name: 'delete_me',
+    run: async (client, interaction, db) => {
+        const ownedChannelId = await db.get(interaction.user.id)
+        if (ownedChannelId !== interaction.channel.id) return interaction.reply({ content: 'Only the owner can delete this channel!', ephemeral: true })
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setLabel('Confirm Delete')
+                    .setStyle(ButtonStyle.Danger)
+                    .setCustomId('delete_channel_confirm')
+            )
+
+        return interaction.reply({ content: 'Are you sure you want to delete this channel? It cannot be undone and your files cannot be recovered.', components: [row], ephemeral: true })
+
+    }
+}
