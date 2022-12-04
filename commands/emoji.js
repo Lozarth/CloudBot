850f1c4b0d4c12c53546c31ef06faa80d542bd65
Lozarth <monkojumboepicness@gmail.com>
2022-12-04 11:27:33 +0000

    Emoji

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,23 +1,23 @@
-const { PermissionsBitField, Utils } = require('discord.js')
+const { PermissionsBitField } = require('discord.js')
 
 module.exports = {
     name: 'emoji',
     run: async (client, interaction, db) => {
-        if (interaction.member.permissions.has(PermissionsBitField.Flags.ManageEmojisAndStickers)) {
-            const emoji = interaction.options.getString('emoji')
-            const name = interaction.options.getString('name')
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageEmojisAndStickers)) return interaction.reply({ content: 'You don\'t have permission to use this command!', ephemeral: true })
+        
+        const emoji = interaction.options.getString('emoji')
+        const name = interaction.options.getString('name')
 
-            const parsedEmoji = Utils.parseEmoji(emoji)
+        const emojiId = emoji.split(':')[2].replace('>', '')
 
-            if (parsedEmoji.id) {
-                const guild = interaction.guild
+        if (emojiId) {
+            const guild = interaction.guild
 
-                const emoji = await guild.emojis.create({ attachment: `https://cdn.discordapp.com/emojis/${parsedEmoji.id}.${parsedEmoji.animated ? 'gif' : 'png'}`, name: name })
+            const emoji = await guild.emojis.create({ attachment: `https://cdn.discordapp.com/emojis/${emojiId}.png`, name: name })
 
-                return interaction.reply({ content: `Created emoji: ${emoji}` })
-            } else {
-                return interaction.reply({ content: 'Invalid emoji!', ephemeral: true })
-            }
+            return interaction.reply({ content: `Created emoji: ${emoji}` })
+        } else {
+            return interaction.reply({ content: 'Invalid emoji!', ephemeral: true })
         }
     }
 }
