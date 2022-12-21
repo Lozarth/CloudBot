2fffde33e47097d94450331a27ef93e4fca72b6f
Lozarth <monkojumboepicness@gmail.com>
2022-12-21 23:16:07 +0000

    Update

--- commands/download.js
+++ commands/download.js
@@ -52,7 +52,7 @@ module.exports = {
 
             for (const [index, format] of formats.entries()) {
                 try {
-                    await interaction.followUp({ content: `${index + 1}/${formats.length}\n**${format.width}x${format.height}** | **${format.qualityLabel}** | **${format.fps}fps** | Video Quality: **${format.quality}** | Audio Quality: **${format.audioQuality}**`, files: [{ attachment: format.url, name: `${videoTitle}.${format.container}` }] })
+                    await interaction.followUp({ content: `${index + 1}/${formats.length}\n**${format.width}x${format.height}** | **${format.qualityLabel}** | **${format.fps}fps** | Video Quality: **${format.quality}** | Audio Quality: **${format.audioQuality}** | File Size: **${format.contentLength / 1000000}mb**`, files: [{ attachment: format.url, name: `${videoTitle}.${format.container}` }] })
                 } catch (error) {
                     console.error(error)
                     await interaction.followUp({ content: `I couldn't send this video!\n\`\`${error}\`\`` })
